ec6a2122f8fd93da823f4d7ffa448b20a72f3687
itsjudyloved <judyloucortes@gmail.com>
2026-05-03 20:41:16 +0800

    Restore Vercel health within function limit

--- vercelApp.js
+++ vercelApp.js
@@ -4,8 +4,8 @@ import path from "node:path";
 process.env.DB_PATH ||= path.join(os.tmpdir(), "sap-mm-alps-review-hub.db");
 
 const [{ createApp }, { getDb }] = await Promise.all([
-  import("../server/src/app.js"),
-  import("../server/src/db.js")
+  import("./server/src/app.js"),
+  import("./server/src/db.js")
 ]);
 
 getDb();
